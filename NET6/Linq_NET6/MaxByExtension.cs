4e49d08e3f3e0e0a44ae061453b3d1b8092593ce
Bill Chung <bill_chungiii@hotmail.com>
2025-01-27 16:41:02 +0800

    加入專案檔案。

---
+++ NET6/Linq_NET6/MaxByExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_NET6
+{
+    public static class MaxByExtension
+    {
+        /// <summary>
+        /// Before .NET 6, OrderByDescending then First
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <returns></returns>
+        public static T CallOrderByDescThenFirst<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector)
+        {
+            return source.OrderByDescending(keySelector).First();
+        }
+
+        /// <summary>
+        /// Before .NET 6, Max then First
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <returns></returns>
+        public static T CallMaxThenFirst<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector)
+        {
+            var max = source.Max(keySelector);
+            return source.First(p => keySelector(p).Equals(max));
+        }
+
+        public static T CallMaxBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector)
+        {
+            return source.MaxBy(keySelector);
+        }
+    }
+}
---
+++ NET6/Linq_NET6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq_NET6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // fake data
+            var people = new List<Person>
+            {
+                new Person { Name = "Alice", Age = 30 },
+                new Person { Name = "Bob", Age = 25 },
+                new Person { Name = "Charlie", Age = 35 }
+            };
+
+            #region MaxBy
+
+            // before .NET 6
+            // var oldestPerson1 = people.OrderByDescending(p => p.Age).First();
+            var oldestPerson1 = people.CallOrderByDescThenFirst(p => p.Age);
+            Console.WriteLine($"[before NET6 (1)] Oldest person: {oldestPerson1.Name}, Age: {oldestPerson1.Age}");
+
+            // before .NET 6
+            //int maxAge = people.Max(p => p.Age);
+            //var oldestPerson2 = people.First(p => p.Age == maxAge);
+            var oldestPerson2 = people.CallMaxThenFirst(p => p.Age);
+            Console.WriteLine($"[before NET6 (2)] Oldest person: {oldestPerson2.Name}, Age: {oldestPerson2.Age}");
+
+            // after .NET 6
+            //  var oldestPerson3 = people.MaxBy(p => p.Age);
+            var oldestPerson3 = people.CallMaxBy(p => p.Age);
+            Console.WriteLine($"[after NET6] Oldest person: {oldestPerson3.Name}, Age: {oldestPerson3.Age}");
+            #endregion
+
+            #region MinBy
+
+            #endregion
+        }
+    }
+
+
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
